e66fe6ad21f3608b77476ba51c93bb787efb4df2
Arun raj <113260000+Arun-cloud-dev@users.noreply.github.com>
2025-07-06 17:20:38 +0530

    update

---
+++ Managementapp/src/app/header.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Header} from '
+
+@Component({
+    selector: 'app-header',
+    // template: `<h1>lorem40</h1>`,
+    templateUrl: './header.html',
+    
+    // standalone: true,
+})
+
+    
+    
+    
+
+
+export class Header {}
